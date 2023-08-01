827b5528bb83f2ce9fa58dc76e57c98fb963c13f
ThaironyAssis <thayronexsuely@hotmail.com>
2023-08-01 16:35:00 +1200

    troquei variavel q tava com nome errado

--- 07/correcao.js
+++ 07/correcao.js
@@ -2,7 +2,7 @@ const nomes = ["Ana", "Joana", "Carlos", "amanda"];
 
 const nomesComA = []
 
-for (let nome of amandas) {
+for (let nome of nomes) {
     const inicial = nome[0];
 
     if (inicial === "A" || inicial === "a") {
